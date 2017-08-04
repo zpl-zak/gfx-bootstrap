52921939d413de080bc5b3945f0f8738540a1562
Dominik Madarasz <madaraszd@mail.ru>
2017-08-04 02:27:39 +0200

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo Downloading the latest version of zpl.h, zpl_gl3w_gen.py modules...
-wget -o zpl.h https://raw.githubusercontent.com/zaklaus/zpl/master/include/zpl.h
-wget -o zpl_gl3w_gen.py https://raw.githubusercontent.com/zaklaus/zpl/master/include/zpl_gl3w_gen.py
+wget https://raw.githubusercontent.com/zaklaus/zpl/master/include/zpl.h -O zpl.h 
+wget https://raw.githubusercontent.com/zaklaus/zpl/master/include/zpl_gl3w_gen.py -O zpl_gl3w_gen.py 
 
 chmod +x ./zpl_gl3w_gen.py
 exec ./zpl_gl3w_gen.py
 
 echo Downloading the latest version of nuklear.h...
-wget -o nuklear.h https://raw.githubusercontent.com/vurtun/nuklear/master/nuklear.h
-wget -o nuklear_sdl_gl3.h https://raw.githubusercontent.com/vurtun/nuklear/master/demo/sdl_opengl3/nuklear_sdl_gl3.h
+wget https://raw.githubusercontent.com/vurtun/nuklear/master/nuklear.h -O nuklear.h 
+wget https://raw.githubusercontent.com/vurtun/nuklear/master/demo/sdl_opengl3/nuklear_sdl_gl3.h -O nuklear_sdl_gl3.h 
 
 
