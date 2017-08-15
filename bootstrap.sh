356b39891e288b290141adb2e8349497310614ea
Dominik Madarasz <madaraszd@mail.ru>
2017-08-16 01:27:18 +0200

    Obsolete v1.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo Downloading the latest version of zpl.h, zpl_gl3w_gen.py modules...
-wget https://raw.githubusercontent.com/zaklaus/zpl/master/include/zpl.h -O zpl.h 
-wget https://raw.githubusercontent.com/zaklaus/zpl/master/include/zpl_gl3w_gen.py -O zpl_gl3w_gen.py 
+wget https://raw.githubusercontent.com/zpl-c/zpl-old/master/include/zpl.h -O zpl.h 
+wget https://raw.githubusercontent.com/zpl-c/zpl-old/master/include/zpl_gl3w_gen.py -O zpl_gl3w_gen.py 
 
 echo Downloading the latest version of nuklear.h...
 wget https://raw.githubusercontent.com/vurtun/nuklear/master/nuklear.h -O nuklear.h 
